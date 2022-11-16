4b9e07ff56e09a31a8288bfbf512bb466f58faf3
MSI\user <eric100789@yahoo.com.tw>
2022-11-16 23:16:16 +0800

    debug - swap(x,y)

--- homework3/hw3_3.py
+++ homework3/hw3_3.py
@@ -6,7 +6,7 @@ import numpy as np
 import cv2
 
 window = None
-ALL_NUM = 7
+ALL_NUM = 8
 NUM_PER_ROW = 4
 
 def MultiplyWeirdNumber(fromnum = 1 ,num = 1):
@@ -28,22 +28,28 @@ def MultiplyWeirdNumber(fromnum = 1 ,num = 1):
     ImgList[num].image = render
     LoadList[num].save("temp.tif")
 
-def DFT_2D(gotnum = 1, num1 = 2, num2 = 3, num3 = 4, num4 = 5):
+def DFT_2D(gotnum = 1, num1 = 2, num2 = 3, num3 = 4, num4 = 5, num5 = 6):
     global LoadList, ImgList
     f1 = np.fft.fft2(np.array(LoadList[gotnum]))
     fshift1 = np.fft.fftshift(f1)
 
-    f2 = cv2.imread('temp.tif',)
+    f2 = cv2.imread('temp.tif')
+    pil_f=Image.open("temp.tif")
     f2 = cv2.cvtColor(f2, cv2.COLOR_BGR2GRAY)
-    dft = cv2.dft(np.float32(f2), flags=cv2.DFT_COMPLEX_OUTPUT)
+    dft = np.fft.fft2(pil_f)#cv2.dft(np.float32(f2), flags=cv2.DFT_COMPLEX_OUTPUT)
     dft_shift = np.fft.fftshift(dft)
-    f2result = 10 * np.log(cv2.magnitude(dft_shift[:, :, 0], dft_shift[:, :, 1]))
+    dft_shift_conj = np.fft.fftshift(np.conjugate(dft))
+    test = 20*np.log(np.abs(dft_shift_conj))
+    f2result = 20 * np.log(np.abs(dft_shift))
+    #f2result= np.conjugate(f2result)
 
     fshift3 = np.fft.ifftshift(fshift1)
+    fshift4 = np.fft.ifftshift(dft_shift_conj)
+    
 
     magnitude_spectrum = 20 * np.log(np.abs(fshift1))
     inverse = np.abs(np.fft.ifft2(fshift3))
-    
+    inverse_conj = 20 * np.log( np.abs(np.fft.ifft2(fshift4)))
 
     dst1 = Image.fromarray(magnitude_spectrum)
     image1 = ImageTk.PhotoImage(dst1)
@@ -61,7 +67,7 @@ def DFT_2D(gotnum = 1, num1 = 2, num2 = 3, num3 = 4, num4 = 5):
 
     for x in range(LoadList[num2].size[0]):
         for y in range(LoadList[num2].size[1]):
-            new_img[x,y] = int(nparr[x][y])
+            new_img[x,y] = int(nparr[y][x])
     render = ImageTk.PhotoImage(LoadList[num2])
     ImgList[num2].config(image = render)
     ImgList[num2].image = render
@@ -71,26 +77,29 @@ def DFT_2D(gotnum = 1, num1 = 2, num2 = 3, num3 = 4, num4 = 5):
     ImgList[num3].config(image = image3)
     ImgList[num3].image = image3
 
-    ishift = np.fft.ifftshift(dft_shift)
-    iimg = cv2.idft(ishift)
-    iresult = cv2.magnitude(iimg[:, :, 0], iimg[:, :, 1])
-
-    nparr = []
-    for x in range(LoadList[num4].size[0]):
+    xyarr = []
+    for x in range(LoadList[num5].size[0]):
         arr = []
-        for y in range(LoadList[num4].size[1]):
-            arr.append(iresult[x][y])
-        nparr.append(arr.copy())
+        for y in range(LoadList[num5].size[1]):
+            arr.append(inverse_conj[y][x])
+        xyarr.append(arr.copy())
 
-    new_img = LoadList[num4].load()
+    new_img = LoadList[num5].load()
 
-    for x in range(LoadList[num4].size[0]):
-        for y in range(LoadList[num4].size[1]):
-            new_img[x,y] = int(nparr[x][y])
+    for x in range(LoadList[num5].size[0]):
+        for y in range(LoadList[num5].size[1]):
+            try:
+                new_img[x,y] = int(xyarr[x][y].real)
+            except:
+                new_img[x,y] = 255
+
     render = ImageTk.PhotoImage(LoadList[num4])
     ImgList[num4].config(image = render)
     ImgList[num4].image = render
-
+    dst5 = Image.fromarray(np.int8(inverse_conj),"L")
+    image5 = ImageTk.PhotoImage(dst5)
+    ImgList[num5].config(image = image5)
+    ImgList[num5].image = image5
 
 def Question3():
     global window, ImgList, render
@@ -107,10 +116,11 @@ def Question3():
         "DFT",
         "Inverse FFT",
         "Inverse DFT",
+        "Get Conj",
         "Multiplied"
     ]
-    explain = tkinter.Label(window, text="Explain:\n傅利葉轉換後的逆轉換\n圖片會一樣\n而共軛會讓圖片倒過來", font=("Arial",18))
-    explain.grid(row=3,column=3)
+    explain = tkinter.Label(window, text="Explain:\n傅利葉轉換後的逆轉換圖片\n會一樣而共軛會讓圖片倒過來", font=("Arial",18),justify='left')
+    explain.grid(row=4,column=0)
 
     for i in range(ALL_NUM):
         try:
@@ -127,10 +137,10 @@ def Question3():
         
     MultiplyWeirdNumber()
     DFT_2D()
-    MultiplyWeirdNumber(5,6)
+    MultiplyWeirdNumber(fromnum=6,num=7)
 
     # size control
-    window.geometry('1220x625')
+    window.geometry('1220x705')
     window.mainloop()
 
 def LoadPicture(pictureName):
